b0650cdff2e915f622e06550ecfb4e5016608fa0
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-01-18 01:20:47 +0300

    Initial Commit

---
+++ TabBarKullanimi/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  TabBarKullanimi
+//
+//  Created by ahmet kardesseven on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        if let tabItems = tabBarController?.tabBar.items {
+            let anasayfaItem = tabItems[0]
+            let ayarlarItem = tabItems[1]
+            
+            anasayfaItem.badgeValue = "1"
+            ayarlarItem.badgeValue = "Update"
+        }
+        
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor.systemIndigo
+        renkDegistir(itemAppearance: appearance.stackedLayoutAppearance)
+        renkDegistir(itemAppearance: appearance.inlineLayoutAppearance)
+        renkDegistir(itemAppearance: appearance.compactInlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = appearance
+        tabBarController?.tabBar.scrollEdgeAppearance = appearance
+        
+    }
+
+    func renkDegistir(itemAppearance:UITabBarItemAppearance){
+        itemAppearance.selected.iconColor = UIColor.systemOrange
+        itemAppearance.selected.titleTextAttributes = [.foregroundColor:UIColor.systemOrange]
+        itemAppearance.selected.badgeBackgroundColor = UIColor.systemMint
+        
+        itemAppearance.normal.iconColor = UIColor.systemOrange
+        itemAppearance.normal.titleTextAttributes = [.foregroundColor:UIColor.white]
+        itemAppearance.normal.badgeBackgroundColor = UIColor.lightGray
+        
+        
+    }
+    
+    
+    
+    
+}
+
